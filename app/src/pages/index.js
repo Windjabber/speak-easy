5df77df86ee21b52f2fa54905f06b043bcc23ba8
Joon-Ho Son <sonjoonh@gmail.com>
2020-02-08 17:22:47 +0000

    Update button styling

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,62 +1,58 @@
 import React, {Component} from "react";
-import { graphql } from "gatsby";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMicrophone } from "@fortawesome/free-solid-svg-icons"
+import {graphql} from "gatsby";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faMicrophone} from "@fortawesome/free-solid-svg-icons"
 import SpeechRecognition from 'react-speech-recognition'
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import * as PropTypes from "prop-types";
+import '../styles/start.css';
 
 
 class Start extends Component {
 
-  state = {
-    transcript: ""
-  };
+    state = {
+        transcript: ""
+    };
 
-  render() {
-    let {
-      transcript,
-      resetTranscript,
-      browserSupportsSpeechRecognition
-    } = this.props;
+    render() {
+        let {
+            transcript,
+            browserSupportsSpeechRecognition,
+            startListening
+        } = this.props;
 
-    if (!browserSupportsSpeechRecognition) {
-      return null
+        if (!browserSupportsSpeechRecognition) {
+            return null
+        }
+        console.log(transcript);
+        return (
+            <>
+                <Layout>
+                    <SEO title="Home"/>
+                    <div style={{justifyContent: 'center', textAlign: 'center', alignItems: 'center'}}>
+                        <h1 style={{fontSize: "60pt"}}>
+                            Let's Riff!
+                        </h1>
+                        <button
+                            style={{border: 'none', marginLeft: 'auto', marginRight: 'auto', backgroundColor: 'white'}}
+                            onClick={() => {
+                                console.log("Starting");
+                                startListening()
+                            }}
+                        >
+                            <FontAwesomeIcon icon={faMicrophone} size={"8x"} color="white" style={{
+                                'borderRadius': '150px',
+                                padding: '0.5em 0.65em',
+                                backgroundColor: '#0CD6B5'
+                            }}
+                                             className="start-button"/>
+                        </button>
+                    </div>
+                </Layout>
+            </>
+        );
     }
-    console.log(transcript);
-    return (
-      <>
-        <Layout>
-          <SEO title="Home"/>
-          <div style={{justifyContent: 'center', textAlign: 'center', alignItems: 'center'}}>
-            <h1 style={{fontSize: "60pt"}}>
-              Let's Riff!
-            </h1>
-            <button
-              style={{border: 'none', marginLeft: 'auto', marginRight: 'auto'}}
-              onClick={() => {
-                resetTranscript();
-              }}
-            >
-              <FontAwesomeIcon icon={faMicrophone} size={"8x"} color="#0CD6B5" style={{
-                'borderRadius': '150px',
-                'boxShadow': '0px 0px 2px #888',
-                'padding': '0.5em 0.65em',
-              }}/>
-            </button>
-          </div>
-        </Layout>
-      </>
-    );
-  }
 }
-
-Start.propTypes = {
-  transcript: PropTypes.any,
-  resetTranscript: PropTypes.any,
-  browserSupportsSpeechRecognition: PropTypes.any
-};
 
 export const query = graphql`
   {
@@ -74,7 +70,7 @@ export const query = graphql`
 `;
 
 const options = {
-  autoStart: true
+    autoStart: false
 };
 
 export default SpeechRecognition(options)(Start);
