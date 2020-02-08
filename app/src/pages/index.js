c997435dd2d9398f7798726800c1c07f49fb58df
Will Burr <williamburr98@gmail.com>
2020-02-08 15:45:29 +0000

    Add initial attempt at ui including title and button

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,78 +1,32 @@
 import React from "react";
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMicrophone } from "@fortawesome/free-solid-svg-icons"
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const Start = ({ data }) => {
-  const { nodes } = data.allMdx;
+const Start = () => {
   return (
     <>
       <Layout>
         <SEO title="Home" />
-        <div className="is-grey is-purple-bg">
-          <div className="row container-small pad-20-t pad-20-b">
-            <div className="col-xs-12 col-md-6 pad-10-l">
-              <h1 className="is-hero-menu margin-0 is-white">
-                🍍 GATSBY PINEAPPLE
-              </h1>
-              <div className="border" />
-              <h4 className="is-hero-sub-text is-white opacity-80">
-                An MDX Boilerplate by SLD.
-              </h4>
-            </div>
-          </div>
-        </div>{" "}
-        <div className="is-light-grey-bg">
-          <div className="row container-small pad-10-t pad-10-b">
-            <div className="col-xs-12 pad-10-l">
-              <h4 className="is-hero-sub-text is-pink opacity-80">
-                Click a presentation to begin.
-              </h4>
-            </div>
-            {nodes.map(item => {
-              const { title, path, desc, location } = item.frontmatter;
-              return (
-                <div className="col-xs-12 col-md-6 col-lg-4 pad-10-l pad-10-r">
-                  <Link to={path + "/slides/0"} className="link-reverse">
-                    <h1>{title}</h1>
-                    <h4>{desc}</h4>
-                    <p className="opacity-50">{location}</p>
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
+        <div style={{justifyContent: 'center', textAlign: 'center', alignItems: 'center'}}>
+          <h1 style={{fontSize: "60pt"}}>
+            Let's Riff!
+          </h1>
+          <button
+            style={{ border: 'none', marginLeft: 'auto', marginRight: 'auto'}}
+            onClick={() => {
+              console.log("Hello world!");
+            }}
+          >
+            <FontAwesomeIcon icon={faMicrophone} size={"8x"} color="#0CD6B5" style={{
+              'borderRadius': '150px',
+              'boxShadow': '0px 0px 2px #888',
+              'padding': '0.5em 0.65em',}}/>
+          </button>
         </div>
       </Layout>
-      <footer className="is-white-bg is-grey margin-2 footer">
-        Site made with{" "}
-        <span role="img" aria-label="love">
-          ❤️
-        </span>{" "}
-        by{" "}
-        <a
-          href="https://sld.codes"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="link"
-        >
-          sld.codes
-        </a>
-        <a
-          className="link is-pink"
-          href="https://www.buymeacoffee.com/lLj8yvRAk"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p>
-            Buy me a coffee{" "}
-            <span role="img" aria-label="coffee">
-              ☕️
-            </span>
-            .
-          </p>
-        </a>
-      </footer>
     </>
   );
 };
